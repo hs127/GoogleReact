b77a2d837b33066575d44cadf1f76d30322ddbac
Hibah Shafi <hibahshafi.hs@gmail.com>
2020-02-15 15:28:15 -0600

    message

---
+++ client/build/precache-manifest.782508965c03490f525435162e4fc4d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a8b9ce5d3191df4dd245",
+    "url": "/static/js/main.17d6db96.chunk.js"
+  },
+  {
+    "revision": "243148357ed6a5721f4a",
+    "url": "/static/js/2.996b92d0.chunk.js"
+  },
+  {
+    "revision": "a8b9ce5d3191df4dd245",
+    "url": "/static/css/main.a439a0be.chunk.css"
+  },
+  {
+    "revision": "3af8356b103cd8deedec4a694e95b3fb",
+    "url": "/index.html"
+  }
+];
